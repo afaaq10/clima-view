20373e3e11db7ab68760b146fc30bd6eda132e14
afaaq10 <afaaqmajeed123@gmail.com>
2024-02-16 22:35:58 +0530

    added weathercard

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,28 @@ import WeatherCard from './components/WeatherCard'; // Import the WeatherCard co
 const App: React.FC = () => {
     const inputRef = React.useRef<HTMLInputElement>(null);
     const [weatherData, setWeatherData] = React.useState(null);
-    const [city, setCity] = React.useState('baramulla');
+    const [city, setCity] = React.useState('');
     const [isLoading, setIsLoading] = React.useState(false);
-
+    const [err, setError] = React.useState("");
     const getData = () => {
-        if (!city) alert("Please enter a valid city name");
-        setIsLoading(true);
+        if (!city) {
+            alert("Please enter a valid city name");
+            return;
+        }
         const APIKey = '546b8e7bc2e42c8c92a2572386d39b0a';
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`;
+        setIsLoading(true);
         axios.get(url)
-            .then(res => setWeatherData(res.data))
-            .catch((err) => console.log(err))
-            .finally(() => setIsLoading(false));
+            .then(res => { setIsLoading(false); setWeatherData(res.data) })
+            .catch((err) => {
+                setError(err.response.data.message);
+                if (err.response && err.response.data && err.response.data.message === 'city not found') {
+                    alert("City not found. Please enter a valid city name.");
+                } else {
+                    alert("An error occurred while fetching data. Please try again later.");
+                }
+                setIsLoading(false);
+            });
     }
 
     React.useEffect(() => {
@@ -32,7 +42,7 @@ const App: React.FC = () => {
                 <button type="button" onClick={getData} className="text-center border bg-yellow-600 p-2 w-32 h-10 rounded-xl">Get weather</button>
             </div>
             {isLoading ? (
-                <div className='mt-9'>Loading...</div>
+                err === 'city not found' ? "" : <div className='mt-9'>Loading...</div>
             ) : (
                 <div className="h-1">
                     {weatherData && <WeatherCard data={weatherData} />}
